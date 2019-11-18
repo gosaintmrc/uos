08037d2e6ebaa34bb8b99ca0c38d2d1c7a696209
caozg <gosaintmrc@aliyun.com>
2019-11-18 10:29:29 +0800

    init

---
+++ uos-backstage-web/src/main/java/com/gosaint/uos/UosBackstageWebApplication.java
@@ -0,0 +1,13 @@
+package com.gosaint.uos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UosBackstageWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UosBackstageWebApplication.class, args);
+    }
+
+}
---
+++ uos-backstage-web/src/test/java/com/gosaint/uos/UosBackstageWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosaint.uos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UosBackstageWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
